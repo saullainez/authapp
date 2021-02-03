a2c7e81ec9610f7e6ce0c47fad37446b34218061
Saúl Laínez <saullainez@hotmail.es>
2021-02-03 14:35:11 -0600

    Guard para proteger ruta

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService:AuthService){
+
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.isAuthenticated$;
+  }
+  
+}
